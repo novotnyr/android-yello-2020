ba902db3f3bdbf0ca100b3e2f6f9f728d45257f0
Robert Novotny <novotnr0@gmail.com>
2020-03-14 21:38:20 +0100

    Pridana entita 'Note'

---
+++ app/src/main/java/com/github/novotnyr/android/yello/Note.java
@@ -0,0 +1,41 @@
+package com.github.novotnyr.android.yello;
+
+import java.util.Date;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class Note {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String description;
+
+    private Long timestamp = new Date().getTime();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
