44272de5cf4540273a059398d3a01d3365c2ad32
Robert Novotny <novotnr0@gmail.com>
2020-03-15 13:02:16 +0100

    Pridany viewholder
    Widgety sa navazuju cez Butterknife

---
+++ app/src/main/java/com/github/novotnyr/android/yello/NoteViewHolder.java
@@ -0,0 +1,22 @@
+package com.github.novotnyr.android.yello;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import butterknife.*;
+
+public class NoteViewHolder extends RecyclerView.ViewHolder {
+    @BindView(android.R.id.text1)
+    TextView text;
+
+    public NoteViewHolder(@NonNull View itemView) {
+        super(itemView);
+        ButterKnife.bind(this, itemView);
+    }
+
+    public void setNote(Note note) {
+        this.text.setText(note.getDescription());
+    }
+}
