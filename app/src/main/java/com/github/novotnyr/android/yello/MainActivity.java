76f418c15d792cefb7d826fca5230f8cb84e92e6
Robert Novotny <novotnr0@gmail.com>
2020-03-15 11:58:16 +0100

    Prepojenie widgetov s layoutom pomocou knižnice _Butterknife_.

--- app/src/main/java/com/github/novotnyr/android/yello/MainActivity.java
+++ app/src/main/java/com/github/novotnyr/android/yello/MainActivity.java
@@ -1,14 +1,34 @@
 package com.github.novotnyr.android.yello;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.*;
+import butterknife.*;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.noteRecyclerView)
+    RecyclerView noteRecyclerView;
+
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        noteRecyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+    }
+
+    @OnClick(R.id.fab)
+    public void onAddNote(View view) {
+        Snackbar.make(view, "Klik!", Snackbar.LENGTH_SHORT).show();
     }
 }
