664fccaf8bb95d1b60b9ac640cb98096a626d5e5
Robert Novotny <novotnr0@gmail.com>
2020-03-15 11:16:50 +0100

    Pridany viewmodel pre pracu s databazou

---
+++ app/src/main/java/com/github/novotnyr/android/yello/NoteViewModel.java
@@ -0,0 +1,29 @@
+package com.github.novotnyr.android.yello;
+
+import android.app.Application;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+public class NoteViewModel extends AndroidViewModel {
+
+    private LiveData<List<Note>> notes;
+
+    private NoteDao noteDao;
+
+    private AppDatabase db;
+
+    public NoteViewModel(@NonNull Application application) {
+        super(application);
+        db = AppDatabase.getDb(application);
+        noteDao = db.noteDao();
+        notes = noteDao.list();
+    }
+
+    public LiveData<List<Note>> getNotes() {
+        return notes;
+    }
+}
