cb5e2edd201493c08a669aa5705f540b30610093
Robert Novotny <novotnr0@gmail.com>
2020-03-15 13:08:44 +0100

    Pridany adapter so systemovym layoutom

---
+++ app/src/main/java/com/github/novotnyr/android/yello/NoteListAdapter.java
@@ -0,0 +1,28 @@
+package com.github.novotnyr.android.yello;
+
+import android.view.*;
+
+import androidx.annotation.*;
+import androidx.recyclerview.widget.ListAdapter;
+
+public class NoteListAdapter extends ListAdapter<Note, NoteViewHolder> {
+
+    public NoteListAdapter() {
+        super(new NoteDiff());
+    }
+
+    @NonNull
+    @Override
+    public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        @LayoutRes int layout = android.R.layout.simple_list_item_1;
+        View itemView = LayoutInflater
+                .from(parent.getContext())
+                .inflate(layout, parent, false);
+        return new NoteViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteViewHolder holder, int position) {
+        holder.setNote(getItem(position));
+    }
+}
